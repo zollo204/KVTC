9b4efee6556924f508fcd58e9ebf5254a95d9cc7
zollo204 <samzollo829@gmail.com>
2026-02-13 21:10:17 +0300

    Refactor app.py for improved structure and readability
    Refactor app.py to improve structure and readability, including organizing imports, defining constants, and ensuring folder creation only occurs once.

--- app.py
+++ app.py
@@ -436,8 +436,9 @@ def student_logout():
     session.clear()
     return redirect(url_for("student_login"))
 
-# -----------------------------
-# RUN
-# -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
